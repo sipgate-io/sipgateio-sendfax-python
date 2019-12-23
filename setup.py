c1aacd32ae6e7192d7f7ea5f2ae400bb4a24952f
Sven Kube <kube@sipgate.de>
2019-12-23 14:53:51 +0100

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ with open('LICENSE') as f:
 setup(
     name='sipgateio-sendfax-python',
     version='0.1.0',
-    description='A demonstration on how to send a fax using the sipgate REST API',
+    description='A demonstration on how to send a fax using sipgate.io',
     long_description=readme,
-    author='sipgate',
-    author_email='',
+    author='sipgate.io Team',
+    author_email='io-team@sipgate.de',
     url='https://github.com/sipgate-io/sipgateio-sendfax-python',
     license=license,
     packages=find_packages(exclude=())
